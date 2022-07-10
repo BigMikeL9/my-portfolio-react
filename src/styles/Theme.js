c6985d3654f521ae4b5bfd36255ec0ec94d8cc70
MikeL Kamel <mikel1832@yahoo.com>
2022-07-10 13:14:47 -0400

    Media queries

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -18,3 +18,36 @@ export const darkTheme = {
     contactMe: "linear-gradient(#061e3b, #040e1b);",
   },
 };
+
+/* 
+Desktop-First approach 👇
+0- 375px            --> Mobile - small                    [max-width: 23.5em]
+0 - 600px           --> Mobile                            [max-width: 37.5em]
+600px - 900px       --> Tablet-Portrait                   [max-width: 56.25em]
+900px - 1200px      --> Tablet-Landscape                  [max-width: 75em]
+[1200px - 1800px]   --> Where our NORMAL STYLES APPLY
+1800px ++           --> Big Desktop                       [min-width: 112.5em]
+
+
+👇 $breakpoint argument choices 👇
+--> mobile-small                 [375px]
+--> mobile                       [600px]
+--> tablet-portrait             [900px]
+--> tablet-landscape            [1200px]
+--> big-desktop                 [1800px]
+
+
+1em = 16px (default root font-size)
+*/
+
+// device size for media query
+export const device = {
+  mobile_S: `(max-width:${"23.5em"})`, // 375px
+  mobile_M: `(max-width:${"26.56em"})`, // 425px
+  mobile_L: `(max-width:${"37.5em"})`, // 600px
+  tablet_Port: `(max-width:${"50.25em"})`, // 900px
+  tablet_Land: `(max-width:${"75em"})`, // 1200px
+  Laptop: `(max-width:${"75em"})`, // 1200px
+  laptopS: `(max-width:${"968px"})`, //
+  laptop: `(max-width:${"1024px"})`,
+};
