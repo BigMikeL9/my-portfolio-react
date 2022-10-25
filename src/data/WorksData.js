cbc0e13abdecbac598882d9575d59ee70fb7e7a9
MikeL Kamel <mikel1832@yahoo.com>
2022-10-25 19:12:25 -0400

    add lazy images

--- src/data/WorksData.js
+++ src/data/WorksData.js
@@ -15,7 +15,7 @@ import DiceGame from "../assets/img/dice-game.jpg";
 import SanitizingEg from "../assets/img/sanitizingeg.jpg";
 
 // -------------------------------------------------------------------
-/* REMEMBER -> still need to add React apps 
+/* TODO -> still need to add React apps 
 
     - Food order app 
     - expense tracker
