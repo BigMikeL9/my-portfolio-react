3e2e498ce31c827b5c85b16267f2e1880d25340a
Mikel Kamel <mike.kamel@tuvsud.com>
2026-08-16 15:17:04 -0400

    feat: update resume. update curtain.

--- src/components/Curtain/Curtain.js
+++ src/components/Curtain/Curtain.js
@@ -1,39 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import {
-  CurtainMainContainer,
-  CurtainS,
-  TypeWriterText,
-} from "./Curtain.style";
-import BongoCat from "../BongoCat/BongoCat";
+import { CurtainMainContainer, CurtainS, TypeWriterText } from './Curtain.style'
+import BongoCat from '../BongoCat/BongoCat'
 
 const CurtainComp = (props) => {
-  // console.log(props);
+    // console.log(props);
 
-  return (
-    <CurtainS {...props}>
-      {props.main && (
-        <CurtainMainContainer>
-          <TypeWriterText>Hello, Stranger.</TypeWriterText>
-          <BongoCat />
-        </CurtainMainContainer>
-      )}
-    </CurtainS>
-  );
-};
+    return (
+        <CurtainS {...props}>
+            {props.main && (
+                <CurtainMainContainer>
+                    <TypeWriterText>Hello, Stranger.</TypeWriterText>
+                    {/* <BongoCat /> */}
+                </CurtainMainContainer>
+            )}
+        </CurtainS>
+    )
+}
 
 const Curtain = (props) => {
-  // console.log(props);
+    // console.log(props);
 
-  return (
-    <>
-      {ReactDOM.createPortal(
-        <CurtainComp {...props} />,
-        document.getElementById("curtain-root")
-      )}
-    </>
-  );
-};
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <CurtainComp {...props} />,
+                document.getElementById('curtain-root')
+            )}
+        </>
+    )
+}
 
-export default Curtain;
+export default Curtain
